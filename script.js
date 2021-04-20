cc4f11562577390246417cbbc9cf2bc082e5a466
corradocoppo <corradocoppolino@gmail.com>
2021-04-20 20:16:11 +0200

    funzionante senza bonus

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
 
-    $(".prev").click(nextImage);
+    $(".next").click(nextImage);
     $(".prev").click(prevImage);
+
+    function nextImage(){
+
+        var activeImg = $("img.active");
+        var activeDot = $("span.dot-attivato");
+
+        activeImg.removeClass("active");
+        activeDot.removeClass("dot-attivato");
+
+        if(activeImg.hasClass("last")){
+            $("img.first").addClass("active");
+            $("span.first").addClass("dot-attivato");
+        }else{
+            activeImg.next("img").addClass("active");
+            activeDot.next("span").addClass("dot-attivato");
+        }
+
+    }
+
+    function prevImage(){
+
+        var activeImg = $("img.active");
+        var activeDot = $("span.dot-attivato");
+
+        activeImg.removeClass("active");
+        activeDot.removeClass("dot-attivato");
+        
+        if(activeImg.hasClass("first")){
+            $("img.last").addClass("active");
+            $("span.last").addClass("dot-attivato");
+        }else{
+            activeImg.prev("img").addClass("active");
+            activeDot.prev("span").addClass("dot-attivato");
+        }
+
+    }
 
 })
