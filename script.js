e89713051d766faccded3b4fd771c3113473af35
corradocoppo <corradocoppolino@gmail.com>
2021-04-21 10:31:46 +0200

    con bonus

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ $(document).ready(function(){
     $(".next").click(nextImage);
     $(".prev").click(prevImage);
 
+    $(".image img").each(function(index){
+
+        var nav = $(".pallini");
+        var active = "";
+
+        if(index === 0){
+            active = "dot-attivato"
+        }
+
+        var tag = '<span class="dot ' + active + '"></span>';
+        nav.append(tag);
+
+    })
+
+    $(".pallini span").on("click",function(){
+        var index = $(this).index();
+        
+        $("span.dot-attivato").removeClass("dot-attivato");
+        $("img.active").removeClass("active");
+        $("img").eq(index).addClass("active");
+        $(this).addClass("dot-attivato");
+    })
+    
     function nextImage(){
 
         var activeImg = $("img.active");
@@ -38,5 +61,7 @@ $(document).ready(function(){
         }
 
     }
+
+   
 
 })
